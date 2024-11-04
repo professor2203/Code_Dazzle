499993dfb05c750a23d2d60f1ffb0ff0417fae93
Abdulah2203 <professorshaha99@gmailcom>
2024-11-04 12:21:34 +0500

    Update in this code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,6 @@ import AfterHero from "../components/AfterHero";
 import Content from "../components/Content";
 import LeadershipCarousel from "../components/LeadershipCarousel";
 import HomeHero from "../components/HomeHero";
-import ChatBots from "../components/ChatBots";
 import Hero from "../components/Hero";
 export default function Home() {
 
